0a097eec0ed946350b2e20c829575f93aeab9af7
gennyble <gen@nyble.dev>
2024-01-25 01:10:47 -0600

    refactor to use globals

--- upload-worker.js
+++ upload-worker.js
@@ -11,80 +11,89 @@ onmessage = function (e) {
 	}
 };
 
+let socket = undefined;
+
+let test_running = false;
+let buffer = undefined;
+let chunk_size = 0;
+let test_length = 0;
+let start_time = undefined;
+let sent_chunks = 0;
+let report_task = undefined;
+
 function uploadTest(server) {
-	const socket = new WebSocket(`ws://${server}/speedtest/upload`);
+	socket = new WebSocket(`ws://${server}/speedtest/upload`);
 
 	socket.addEventListener("open", (event) => {
 		console.log("[worker::ws] connection opened!");
 	});
 
-	let buffer = undefined;
-	let test_running = false;
-	let chunk_size = 0;
-	let test_length = 0;
-	let test_started = undefined;
-	let sent_chunks = 0;
-
-	setInterval(() => {
-		console.log(`${sent_chunks} - ${socket.bufferedAmount}`);
-	}, 500);
-
-	let timeout_handle = undefined;
-	let try_send = () => {
-		if (!test_running) {
-			return;
-		}
-
-		if (Date.now() - test_started > test_length * 1000) {
-			console.log("stopped from running overtime!");
-			test_running = false;
-		}
-
-		if (socket.bufferedAmount > buffer.length) {
-			console.log(`${socket.bufferedAmount} in buffer, waiting 10ms`);
-			timeout_handle = setTimeout(() => { try_send() }, 10);
-		} else {
-			socket.send(buffer);
-			sent_chunks++;
-			try_send();
-		}
-	};
-
 	socket.addEventListener("message", (event) => {
-		console.log("[worker::ws] got message!");
-
-		console.log(event.data);
 		const json = JSON.parse(event.data);
+
 		if (json["type"] === "upload-start") {
 			chunk_size = json["chunkSize"];
-			// the server tells us to stop, but we don't want to just keep sending.
+			// the server tells us to stop, but we don't want to just keep
+			// sending in case of extreme latency. it's a failsafe.
 			test_length = json["length"] + 2;
 			test_running = true;
-			test_started = Date.now();
+			start_time = Date.now();
 
-			console.log("[worker] making random data...");
-			buffer = new Uint8Array(1024 * chunk_size);
-			for (let i = 0; i < buffer.length; ++i) {
-				buffer[i] = Math.floor(Math.random() * 256);
-			}
-			console.log("[worker] made data!");
-
+			make_data();
 			try_send();
+			start_report();
 		} else if (json["type"] === "upload-stop") {
 			test_running = false;
 			let chunk_count = json["chunkCount"];
 			let delta = json["delta"];
 
+			// the server gives us the final chunk count and delta value. the
+			// global values are used for progress reports.
 			postMessage({ "type": "upload-stop", "chunkSize": chunk_size, "chunkCount": chunk_count, "delta": delta });
 			socket.close();
 		}
 	});
 }
 
-/*
-data format:
-{
-	"chunkCount": number,
-	"chunkSize": number // size in kilobytes
+function try_send() {
+	if (!test_running) {
+		return;
+	}
+
+	if (Date.now() - start_time > test_length * 1000) {
+		console.log("[worker] stopped from running overtime!");
+		test_running = false;
+	}
+
+	// waiting until the buffer is empty worries me in a "that'll throttle
+	// the connection" kind of way. I'm sure this will, too, but it'll have
+	// less of a chance probably. keeps it stocked with at least one chunk.
+	if (socket.bufferedAmount > buffer.length) {
+		setTimeout(() => { try_send() }, 10);
+	} else {
+		socket.send(buffer);
+		sent_chunks++;
+		try_send();
+	}
 }
-*/
+
+function make_data() {
+	buffer = new Uint8Array(1024 * chunk_size);
+	for (let i = 0; i < buffer.length; ++i) {
+		buffer[i] = Math.floor(Math.random() * 256);
+	}
+}
+
+function start_report() {
+	report_task = setInterval(() => {
+		let current_time = Date.now();
+
+		postMessage({
+			"type": "upload-progress",
+			"start": start_time,
+			"current": current_time,
+			"chunkCount": sent_chunks,
+			"chunkSize": chunk_size
+		});
+	}, 250);
+}
